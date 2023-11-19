a9c7a5b653bcd48d47189bc1cde8b393faab4594
Kyryl R <kyryl.ryabov@gmail.com>
2023-11-19 15:50:18 +0200

    Finished lab6

--- lab6/examples/serial.rs
+++ lab6/examples/serial.rs
@@ -1,65 +1,11 @@
 use lab1::common::input_size;
-
-fn process_initialization() -> (Vec<f64>, usize, f64) {
-    let eps = 0.0;
-
-    // Input grid size and accuracy
-    let size = input_size().unwrap() as usize;
-
-    // Initialize matrix
-    let matrix = vec![0.0; size * size];
-    (matrix, size, eps)
-}
-
-fn dummy_data_initialization(matrix: &mut [f64], size: usize) {
-    for i in 0..size {
-        for j in 0..size {
-            matrix[i * size + j] = if i == 0 || i == size - 1 || j == 0 || j == size - 1 {
-                100.0
-            } else {
-                0.0
-            };
-        }
-    }
-}
-
-fn result_calculation(matrix: &mut [f64], size: usize, eps: f64) -> usize {
-    let mut iterations = 0;
-    let mut dmax;
-
-    loop {
-        dmax = 0.0;
-        for i in 1..(size - 1) {
-            for j in 1..(size - 1) {
-                let temp = matrix[i * size + j];
-                matrix[i * size + j] = 0.25
-                    * (matrix[i * size + j + 1]
-                        + matrix[i * size + j - 1]
-                        + matrix[(i + 1) * size + j]
-                        + matrix[(i - 1) * size + j]);
-                let dm = (matrix[i * size + j] - temp).abs();
-                if dm > dmax {
-                    dmax = dm;
-                }
-            }
-        }
-        iterations += 1;
-        if dmax <= eps {
-            break;
-        }
-    }
-    iterations
-}
+use lab6::serial::lab6_main;
 
 fn main() {
     println!("Serial Gauss-Seidel algorithm in Rust");
 
-    let (mut matrix, size, eps) = process_initialization();
-    dummy_data_initialization(&mut matrix, size);
+    // Input grid size and accuracy
+    let size = input_size().unwrap() as usize;
 
-    let t_start = std::time::Instant::now();
-    let iterations = result_calculation(&mut matrix, size, eps);
-    println!("Time of execution: {:?}", t_start.elapsed());
-
-    println!("Number of iterations: {}", iterations);
+    lab6_main(size);
 }
--- lab6/src/serial/mod.rs
+++ lab6/src/serial/mod.rs
@@ -1,1 +1,57 @@
+fn process_initialization(size: usize) -> (Vec<f64>, usize, f64) {
+    let eps = 0.0;
 
+    // Initialize matrix
+    let matrix = vec![0.0; size * size];
+    (matrix, size, eps)
+}
+
+fn dummy_data_initialization(matrix: &mut [f64], size: usize) {
+    for i in 0..size {
+        for j in 0..size {
+            matrix[i * size + j] = if i == 0 || i == size - 1 || j == 0 || j == size - 1 {
+                100.0
+            } else {
+                0.0
+            };
+        }
+    }
+}
+
+fn result_calculation(matrix: &mut [f64], size: usize, eps: f64) -> usize {
+    let mut iterations = 0;
+    let mut dmax;
+
+    loop {
+        dmax = 0.0;
+        for i in 1..(size - 1) {
+            for j in 1..(size - 1) {
+                let temp = matrix[i * size + j];
+                matrix[i * size + j] = 0.25
+                    * (matrix[i * size + j + 1]
+                        + matrix[i * size + j - 1]
+                        + matrix[(i + 1) * size + j]
+                        + matrix[(i - 1) * size + j]);
+                let dm = (matrix[i * size + j] - temp).abs();
+                if dm > dmax {
+                    dmax = dm;
+                }
+            }
+        }
+        iterations += 1;
+        if dmax <= eps {
+            break;
+        }
+    }
+    iterations
+}
+
+pub fn lab6_main(size: usize) {
+    let (mut matrix, size, eps) = process_initialization(size);
+    dummy_data_initialization(&mut matrix, size);
+
+    let t_start = std::time::Instant::now();
+    let iterations = result_calculation(&mut matrix, size, eps);
+    println!("Time of execution: {:?}", t_start.elapsed());
+    println!("Serial Iterations: {}", iterations);
+}
